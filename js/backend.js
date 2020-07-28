ebf86fc4ca7d21bdd62edf8a72c2a48d43b33211
Ilya <ilya.shcoder@gmail.com>
2020-07-28 17:09:59 +0300

    Добавляю модуль backend.js
    Модуль отвечает за работу с сетевыми запросами: получение и отправка данных на тестовый сервер. Осталось написать обработчик на форму для отправки данных с формы на сервер.

--- js/backend.js
+++ js/backend.js
@@ -1,2 +1,73 @@
 "use strict";
-(function() {})();
+(function() {
+    const URL = {
+        download: 'https://javascript.pages.academy/keksobooking/data',
+        upload: 'https://javascript.pages.academy/keksobooking',
+    };
+
+    const ServerCode = {
+        SUCCESS: 200,
+        BAD_REQUEST: 400,
+        NOT_FOUND: 404,
+        ENTERNAL_ERROR: 500
+    };
+
+    const getXHR = function (onSuccess, onError) {
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+
+        xhr.addEventListener('load', function() {
+            let error;
+            switch (xhr.status) {
+                case ServerCode.SUCCESS:
+                    onSuccess(xhr.response);
+                    break;
+                case ServerCode.BAD_REQUEST:
+                    error = 'Неверный запрос';
+                    break;
+                case ServerCode.NOT_FOUND:
+                    error = 'Ничего не найдено';
+                    break;
+                case ServerCode.ENTERNAL_ERROR:
+                    error = 'Ошибка сервера';
+                    break;
+                default:
+                    error = `Статус ответа ${xhr.status} ${xhr.statusText}`;
+            }
+            if (error) {
+                onError(error);
+            } 
+        });
+
+        xhr.addEventListener('error', function () {
+            onError('Произошла ошибка соединения');
+          });
+        
+
+        xhr.timeout = 10000;
+        xhr.addEventListener('timeout', function () {
+        onError(`Запрос не успел выполниться за ${xhr.timeout}`);
+        });
+    
+        return xhr;
+    };
+
+    const getData = (onSuccess, onError) => {
+        let xhr = getXHR(onSuccess, onError);
+
+        xhr.open('GET', URL.download);
+        xhr.send();
+    };
+
+    const sendData = (formData, onSuccess, onError) => {
+        let xhr = getXHR(onSuccess, onError);
+        xhr.open('POST', URL.upload)
+        xhr.send(formData);
+    };
+
+
+    window.backend = {
+        load: getData,
+        send: sendData,
+      };
+})();
